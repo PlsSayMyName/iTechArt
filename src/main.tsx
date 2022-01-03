4bb4c902cbd8cfbb40a2ea00d1bfd1afa42b0b5e
Dmitrij Menkowskij <madaraisback@gmail.com>
2022-01-03 17:07:06 +0300

    remove unused items

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import "./styles/main.css";
 import "./styles/main.scss";
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
-import imgSmall from "images/testSmall.png"; // start-path is 'images' because we have an alias 'images' in webpack.common.js
-import imgCamera from "images/camera.svg";
-import { Component, StrictMode } from "react";
+// start-path is 'images' because we have an alias 'images' in webpack.common.js
+import { Component } from "react";
 import ReactDom from "react-dom";
-import style from "./styles/main.module.css";
-import someTypeScript from "./someTypeScript";
 
 interface AppProps {
   nothing: boolean;
@@ -20,9 +17,6 @@ class AppContainer extends Component<AppProps, AppState> {
 
   constructor(props: AppProps) {
     super(props);
-    this.state = {
-      title: someTypeScript("Test-block for css-modules"),
-    };
     // test class-dead-code
     const goExlcude = true;
     if (!goExlcude) {
@@ -32,22 +26,18 @@ class AppContainer extends Component<AppProps, AppState> {
 
   render() {
     return (
-      <StrictMode>
-        <div className="test-block">
-          <h2 className={style.mainTitle}>{this.state.title}</h2>
-        </div>
-        <div className={["test-block", style.background].join(" ")}>
-          <h2>Test-block for assets-module (previous url-loader)</h2>
-          <img src={imgSmall} alt="smallImage" />
-        </div>
-        {/*  or it can be
-          <img src='/src/images/testSmall.png' alt="smallImage"></img>
-        */}
-        <div className={["test-block", style.svgBackground].join(" ")}>
-          <h2>Test-block for assets-module (svg-url-loader)</h2>
-          <img src={imgCamera} alt="small_SVG_Image" />
-        </div>
-      </StrictMode>
+      <header>
+        <div>Tect</div>
+        <nav>
+          <ul>
+            <li>345</li>
+            <li>564</li>
+            <li>kjewfgh</li>
+            <li>23476</li>
+            <li>fghwe</li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
